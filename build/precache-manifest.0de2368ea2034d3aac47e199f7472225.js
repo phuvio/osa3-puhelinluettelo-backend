c2c6f380230c2b87887786cacb0b65d6b6aa8ee0
phuvio <petteri.huvio@gmail.com>
2022-06-26 17:48:06 +0300

    uibuild

---
+++ build/precache-manifest.0de2368ea2034d3aac47e199f7472225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb8b6b3f1bb35ef310b",
+    "url": "/static/css/main.c16f9842.chunk.css"
+  },
+  {
+    "revision": "adb8b6b3f1bb35ef310b",
+    "url": "/static/js/main.adb8b6b3.chunk.js"
+  },
+  {
+    "revision": "ade382d22470ff1a8a3b",
+    "url": "/static/js/1.ade382d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e64faab9393db35706fc994ff748bf8",
+    "url": "/index.html"
+  }
+];
